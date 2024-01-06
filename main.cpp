013f96817e159289d3de451ac46263cd1e9cb41a
TGyAkos <t.gyula.akos@gmail.com>
2024-01-06 18:33:57 +0100

    cleaned up main.cpp, extended CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -2,28 +2,12 @@
 #include <iostream>
 
 #include "parseFile.h"
+#include "run.h"
 
 
 int main() {
-    std::string a = "sdfm;msdf;";
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << std::format("Hello, {}!", a) << std::endl;
-    utils::parseFile parsefile("asd.txt");
-    const expense testExpense = {"123", essential, other,2, "test test test"};
-    std::list<expense> testExpenses;
-    testExpenses.push_back(testExpense);
-    parsefile.setExpenses(testExpenses);
-    parsefile.appendToFile();
-    for (const auto& c: parsefile.getExpenses()) {
-        std::cout << c.date;
-        std::cout << c.type;
-        std::cout << c.category;
-        std::cout << c.amount;
-        std::cout << c.comment;
-    }
+    runtime::run runObject;
+    runObject.runMainLoop();
     return 0;
 }
 
-std::string a() {
-    return "Hello, World!";
-}
